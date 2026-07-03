55881295701389ddc83216fa59c4cd0a0acfaf9c
multibillionaire202505-arch <multibillionaire202505@gmail.com>
2026-07-02 22:05:26 -0700

    Update image path for 'Red-red' dish
    Replaced the image URL for the dish 'Red-red' with a local path.

--- worldmeals/meals.js
+++ worldmeals/meals.js
@@ -3769,9 +3769,7 @@ window.WORLD_MEALS_DATABASE = [
     "carbs": 56,
     "fat": 18,
     "fiber": 5,
-    "image": "https://images.unsplash.com/photo-1660675134062-7d3bbb340608?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5ODU5NjB8MHwxfHNlYXJjaHwxfHxSZWQlMjBSZWQlMjBHaGFuYXxlbnwwfDB8fHwxNzgyNjk1MjIzfDA&ixlib=rb-4.1.0&q=80&w=1080"
-  },
-  {
+     "image": "/images/dishes/Red-red.jpg"
     "id": 82,
     "name": "Kenkey and Fish",
     "country": "Ghana",
