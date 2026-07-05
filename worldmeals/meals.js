770fcba2983a3b1b078c1d025b5068c2a3ff9a98
multibillionaire202505-arch <multibillionaire202505@gmail.com>
2026-07-04 20:59:37 -0700

    Update meal image URL for Pastilla dish

--- worldmeals/meals.js
+++ worldmeals/meals.js
@@ -4766,7 +4766,7 @@ window.WORLD_MEALS_DATABASE = [
     "carbs": 27,
     "fat": 8,
     "fiber": 5,
-    "image": "https://images.unsplash.com/photo-1661083098412-054431ab7112?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5ODU5NjB8MHwxfHNlYXJjaHwxfHxQYXN0aWxsYSUyME1vcm9jY28lMjBmb29kfGVufDB8MHx8fDE3ODI2OTUyNTN8MA&ixlib=rb-4.1.0&q=80&w=1080"
+    "image": "/images/dishes/Pastilla.jpg"
   },
   {
     "id": 104,
