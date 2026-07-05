3d01dd9a17daec27cf8c3c3c81761a9d02a65ad6
multibillionaire202505-arch <multibillionaire202505@gmail.com>
2026-07-05 12:34:49 -0700

    Update image URL for Sauerbraten dish

--- worldmeals/meals.js
+++ worldmeals/meals.js
@@ -11125,7 +11125,7 @@ window.WORLD_MEALS_DATABASE = [
     "carbs": 81,
     "fat": 25,
     "fiber": 5,
-    "image": "https://images.unsplash.com/photo-1599921841143-819065a55cc6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5ODU5NjB8MHwxfHNlYXJjaHwxfHxTYXVlcmJyYXRlbiUyMEdlcm1hbnl8ZW58MHwwfHx8MTc4MjY4MzQ0OXww&ixlib=rb-4.1.0&q=80&w=1080"
+    "image": "/images/dishes/Sauerbraten.jpg"
   },
   {
     "id": 234,
