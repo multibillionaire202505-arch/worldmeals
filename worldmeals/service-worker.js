1029511ce1a681649b3e9a9f68a2ca27ee9d032e
multibillionaire202505-arch <multibillionaire202505@gmail.com>
2026-07-26 20:15:57 -0700

    Add service worker for caching and offline support

---
+++ worldmeals/service-worker.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = "worldmeals-v1";
+
+const APP_SHELL = [
+  "/",
+  "/index.html",
+  "/app.html",
+  "/manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseCopy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseCopy);
+        });
+
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request).then(cachedResponse => {
+          return cachedResponse || caches.match("/app.html");
+        })
+      )
+  );
+});
